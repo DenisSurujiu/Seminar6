3f1f9e8b5c5a4bd4de604299fc6dbb65a118e071
Denis Surujiu <surujiudenis254@gmail.com>
2023-10-14 11:25:31 +0300

    Решение 1-ой задачи

---
+++ C#_6.1.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace C__6._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+Console.WriteLine("Введите элементы(через пробел): ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+        }
+    }
+}
